3fc498f747f678d845b4cdf5af263ae7475e9983
RadCon4 <a.conradharrison@gmail.com>
2017-10-24 15:52:38 -0500

    Add files via upload

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,181 @@
-// Setting up folder system in GitHub
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Rick Sanchez',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728',
+		scores: [
+			'5',
+			'5',
+			'4',
+			'1',
+			'2',
+			'5',
+			'5',
+			'2',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Morty Smith',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/7/7e/Screenshot_2015-09-24_at_1.42.22_PM.png/revision/latest?cb=20150924181727',
+		scores: [
+			'1',
+			'3',
+			'1',
+			'2',
+			'2',
+			'4',
+			'3',
+			'1',
+			'4',
+			'4'
+		]
+	},
+	{
+		name: 'Beth Smith',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/5/58/Beth_Smith.png/revision/latest?cb=20151204220729',
+		scores: [
+			'4',
+			'4',
+			'3',
+			'2',
+			'4',
+			'4',
+			'4',
+			'3',
+			'2',
+			'2'
+		]
+	},
+	{
+		name: 'Jerry Smith',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/f/f1/Jerry_Smith.png/revision/latest?cb=20160923151111',
+		scores: [
+			'3',
+			'3',
+			'2',
+			'3',
+			'1',
+			'2',
+			'1',
+			'2',
+			'4',
+			'4'
+		]
+	},
+	{
+		name: 'Summer Smith',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_is_cool.jpeg/revision/latest?cb=20160919183158',
+		scores: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Birdperson',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/b/b1/Birdperson.png/revision/latest?cb=20140415133633',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jessica',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/e/e8/Jessica.png/revision/latest?cb=20150706222334',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Tammy Gueterman',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/0/08/Tammy.png/revision/latest?cb=20140523063749',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+		{
+		name: 'Frank Palicky',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/a/a7/Frankp.jpg/revision/latest?cb=20150908095310',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Keara',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/3/3c/Kiara.png/revision/latest?cb=20171002225423',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Ice-T',
+		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/2/26/Ice-T.PNG/revision/latest?cb=20150902072346',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
