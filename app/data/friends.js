713d886984bdb896a742abdae9591ff09882cdfe
Conrad Harrison <a.conradharrison@gmail.com>
2017-11-21 09:57:32 -0600

    Updated friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -20,7 +20,7 @@ var friendsArray = [
 		name: 'Morty Smith',
 		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/7/7e/Screenshot_2015-09-24_at_1.42.22_PM.png/revision/latest?cb=20150924181727',
 		scores: [
-			'1',
+			'2',
 			'3',
 			'1',
 			'2',
@@ -52,7 +52,7 @@ var friendsArray = [
 		name: 'Jerry Smith',
 		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/f/f1/Jerry_Smith.png/revision/latest?cb=20160923151111',
 		scores: [
-			'3',
+			'1',
 			'3',
 			'2',
 			'3',
@@ -84,8 +84,8 @@ var friendsArray = [
 		name: 'Birdperson',
 		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/b/b1/Birdperson.png/revision/latest?cb=20140415133633',
 		scores: [
-			'1',
-			'1',
+			'2',
+			'4',
 			'5',
 			'1',
 			'3',
@@ -100,48 +100,48 @@ var friendsArray = [
 		name: 'Jessica',
 		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/e/e8/Jessica.png/revision/latest?cb=20150706222334',
 		scores: [
-			'5',
-			'4',
-			'4',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
 			'2',
 			'4',
-			'4',
 			'3',
-			'2',
 			'3',
-			'3'
+			'4'
 		]
 	},
 	{
 		name: 'Tammy Gueterman',
 		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/0/08/Tammy.png/revision/latest?cb=20140523063749',
 		scores: [
-			'5',
+			'4',
+			'3',
+			'4',
 			'2',
-			'3',
-			'1',
-			'5',
-			'1',
-			'3',
-			'1',
-			'1',
-			'5'
+			'2',
+			'2',
+			'2',
+			'2',
+			'4',
+			'4'
 		]
 	},
-		{
+	{
 		name: 'Frank Palicky',
 		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/a/a7/Frankp.jpg/revision/latest?cb=20150908095310',
 		scores: [
-			'5',
+			'1',
+			'4',
+			'3',
+			'1',
 			'2',
-			'3',
-			'1',
 			'5',
+			'5',
+			'4',
 			'1',
-			'3',
-			'1',
-			'1',
-			'5'
+			'2'
 		]
 	},
 	{
@@ -149,31 +149,31 @@ var friendsArray = [
 		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/3/3c/Kiara.png/revision/latest?cb=20171002225423',
 		scores: [
 			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5',
-			'5'
+			'4',
+			'4',
+			'4',
+			'2',
+			'3',
+			'3',
+			'2',
+			'3',
+			'2'
 		]
 	},
 	{
 		name: 'Ice-T',
 		photo: 'https://vignette.wikia.nocookie.net/rickandmorty/images/2/26/Ice-T.PNG/revision/latest?cb=20150902072346',
 		scores: [
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1',
-			'1'
+			'4',
+			'3',
+			'5',
+			'2',
+			'3',
+			'2',
+			'4',
+			'3',
+			'4',
+			'3'
 		]
 	}
 ];
